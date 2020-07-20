14ec59f8c540b06fb8b714e0e0b05ccbdb66d025
Hector <hector@parrotlabs.com.mx>
2020-07-19 19:38:45 -0500

    Remove active mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,6 @@ Rails.application.configure do
 
   config.log_tags = [ :request_id ]
 
-  config.action_mailer.perform_caching = false
-
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
